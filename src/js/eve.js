4f6e79133054a9537cd29551b1feb63aa673be93
axelolea <axelcedilloolea@gmail.com>
2023-06-05 21:06:00 -0600

    Paradigma OOP

---
+++ src/js/eve.js
@@ -0,0 +1,37 @@
+const elemento = prompt("Ingresa el elemento conel que evolucionara Eevee: ").toLocaleLowerCase()
+
+let evolucion;
+
+switch(elemento){
+    case agua:
+        evolucion = "Vaporeon"
+        break;
+    case electrico:
+        evolucion = "Jolteon"
+        break;
+    case fuego:
+        evolucion = "Flareon"
+        break;
+    case psiquico:
+        evolucion = "Espeon"
+        break;
+    case siniestro:
+        evolucion = "Umbreon"
+        break;
+    case planta:
+        evolucion = "Leafeon"
+        break;
+    case hielo:
+        evolucion = "Glaceon"
+        break;
+    case hada:
+        evolucion = "Sylveon"
+        break;
+}
+
+if(evolucion){
+    alert("Eevee evoluciono a " + evolucion)
+}
+else{
+    alert("Opcion incorrecta")
+}
